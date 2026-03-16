7215acd692141ab390031b0579d6a66c03f821ba
lou-bou <boussaid.louai25@gmail.com>
2026-03-16 12:02:02 +0100

    1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,4 +1,17 @@
-const factorial = function() {
+const factorial = function (n) {
+    if (n < 0 || !Number.isInteger(n)) {
+        return
+    }
+
+    if (n == 0) {
+        return 1;
+    }
+
+    if (n == 1) {
+        return n;
+    }
+
+    return n * factorial(n - 1);
 
 };
 
