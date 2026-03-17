005d24c512eea2fe42b177fbd8c264eb2a20f5d8
lou-bou <boussaid.louai25@gmail.com>
2026-03-17 09:28:57 +0100

    2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,28 @@
-const contains = function() {
-  
+const contains = function (object, value) { 
+    
+    if (Object.keys(object).length > 0) {
+        for (const key in object) {
+            if (typeof object[key] != 'object' || object[key] === null || Array.isArray(object[key])) {
+            /* 2 edge cases:
+                js treats null as object
+                js treats array as object
+                in this condition they're treated as normal values
+            */
+                if (Object.is(object[key], value)) {
+                    return true;
+                }
+            } else {
+                let status = contains(object[key], value);
+                if (status == true) { // if the returned value from the recursive function is true, return it again in the outer function
+                    // this is to avoid returning false in outer function after finding the value in an inner function
+                    return status;
+                }
+            }
+        }
+    }
+
+    return false;
+    
 };
   
 // Do not edit below this line
